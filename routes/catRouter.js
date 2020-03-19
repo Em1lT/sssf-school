c35f566de2824c651fa7fd45ba48394e296601eb
Emil Toivainen <emilanton.toivainen@gmail.com>
2020-03-19 18:26:23 +0200

    Added multer

--- routes/catRouter.js
+++ routes/catRouter.js
@@ -1,12 +1,36 @@
 const express = require('express');
 const router = express.Router();
+const multer = require('multer')
+
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'uploads')
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.fieldname + '-' + Date.now())
+    }
+})
+
+const upload = multer({
+    storage: storage
+})
+
 const catController = require('../controllers/catController');
 
 router.get('/', catController.cat_list_get);
 
-router.post('/', (req, res) => {
-    res.send('With this endpoint you can add cats.');
-});
+
+router.post('/uploadfile', upload.single('myFile'), (req, res, next) => {
+    console.log(req.file);
+    const file = req.file
+    if (!file) {
+      const error = new Error('Please upload a file')
+      error.httpStatusCode = 400
+      return next(error)
+    }
+      res.send(file)
+    
+  })
 
 router.put('/', (req, res) => {
     res.send('With this endpoint you can edit cats.');
