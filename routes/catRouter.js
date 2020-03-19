5c811edd2329bbb42139cac9cf92cc17a7aa67f6
Emil Toivainen <emilanton.toivainen@gmail.com>
2020-03-19 20:31:41 +0200

    completed routing

--- routes/catRouter.js
+++ routes/catRouter.js
@@ -3,12 +3,12 @@ const router = express.Router();
 const multer = require('multer')
 
 var storage = multer.diskStorage({
-    destination: function (req, file, cb) {
+    destination: (req, file, cb) => {
         cb(null, './uploads/')
     },
-    filename: function (req, file, cb) {
+    filename: (req, file, cb) => {
         const ext = file.mimetype.split('/')[1];
-        next(null, file.fieldname + '-' + Date.now() + '.'+ext);
+        cb(null, file.fieldname + '-' + Date.now() + '.'+ext);
     }
 })
 
@@ -21,15 +21,10 @@ const catController = require('../controllers/catController');
 router.get('/', catController.cat_list_get);
 
 
-router.post('/uploadfile', upload.single('myFile'), (req, res, next) => {
+router.post('/', upload.single('cat'), (req, res) => {
     console.log(req.file);
-    const file = req.file
-    if (!file) {
-      const error = new Error('Please upload a file')
-      error.httpStatusCode = 400
-      return next(error)
-    }
-      res.send(file)
+    console.log(req.body);
+    res.send("With this endpoint you can edit cats.");
     
   })
 
